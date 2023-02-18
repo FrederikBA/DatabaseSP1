bc8e8ce803bf7aad8e729e7e6a1f70e0e94d3ae8
Julius Krüger Madsen <1230150922@protonmail.com>
2023-02-18 10:19:47 +0100

    Delete query

---
+++ queries/SQLDeleteQuery.sql
@@ -0,0 +1,5 @@
+/****** Script for DELETING everything  ******/
+DELETE FROM [Library].[dbo].[Book]
+DELETE FROM [Library].[dbo].[Loan]
+DELETE FROM [Library].[dbo].[Magazine]
+DELETE FROM [Library].[dbo].[Patron]
